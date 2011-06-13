ba08a81196c738c50d42787adbb81b9ec8c3f0cf
Colin McCann <colindmccann@gmail.com>
2011-06-13 12:45:24 -0400

    basic layout

--- gen/sail/evoroom/android/R.java
+++ gen/sail/evoroom/android/R.java
@@ -14,8 +14,14 @@ public final class R {
         public static final int background=0x7f020000;
         public static final int icon=0x7f020001;
     }
+    public static final class id {
+        public static final int group_viewer=0x7f050000;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int evolutionconceptstab=0x7f030000;
+        public static final int fieldguidetab=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int observationtab=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
