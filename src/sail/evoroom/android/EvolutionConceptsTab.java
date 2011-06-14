20793b1d92d35120374bd370a86d17da4f3880b5
Armin Krauss <mackrauss@gmail.com>
2011-06-14 12:55:04 -0400

    Cleaned up the code since static content is in XML file.

--- src/sail/evoroom/android/EvolutionConceptsTab.java
+++ src/sail/evoroom/android/EvolutionConceptsTab.java
@@ -1,20 +1,13 @@
 package sail.evoroom.android;
 
 import android.app.Activity;
-import android.app.TabActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+
 
 public class EvolutionConceptsTab extends Activity {
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.evolutionconceptstab);
-
-		TextView textBox = null;
-
-		textBox = (TextView) findViewById(R.id.evoConceptsText);
-		textBox.setText("TEST");
-
 	}
 }
