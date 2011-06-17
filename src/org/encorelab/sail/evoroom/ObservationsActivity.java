b2570afaf6a6cd357b8bf7743ce909faed9f101c
Matt Zukowski <matt@zukowski.ca>
2011-06-17 02:24:15 -0400

    working multi-view code

--- src/org/encorelab/sail/evoroom/ObservationsActivity.java
+++ src/org/encorelab/sail/evoroom/ObservationsActivity.java
@@ -1,26 +1,49 @@
 package org.encorelab.sail.evoroom;
 
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class ObservationsActivity extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        FragmentManager.enableDebugLogging(true);
-        
         setContentView(R.layout.observations);
         
-        
-        
-        
-        Intent intent = new Intent(); 
-        intent.setClass(this, ObservationsEntryFragment.class); 
-        startActivity(intent);
-        
+        setupModeButtons();
+        setupEntryView();
+        setupCloudView();
     }
+	
+	private void setupModeButtons() {
+		Button button_to_entry = (Button) findViewById(R.id.button_to_entry);
+        button_to_entry.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                findViewById(R.id.observations_entry_view).setVisibility(View.VISIBLE);
+                findViewById(R.id.observations_cloud_view).setVisibility(View.GONE);
+            }
+        });
+        
+        Button button_to_cloud = (Button) findViewById(R.id.button_to_cloud);
+        button_to_cloud.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	findViewById(R.id.observations_entry_view).setVisibility(View.GONE);
+                findViewById(R.id.observations_cloud_view).setVisibility(View.VISIBLE);
+            }
+        });
+	}
+	
+	private void setupEntryView() {
+		Spinner colourPicker = (Spinner) findViewById(R.id.colour_picker);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	            this, R.array.colours, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    colourPicker.setAdapter(adapter);
+	}
+	
+	private void setupCloudView() {
+	}
 }
