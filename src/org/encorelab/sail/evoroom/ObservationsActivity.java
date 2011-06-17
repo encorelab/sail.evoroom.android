fc18bbe14cb601eb952cb8aa1c5624885cec8286
Matt Zukowski <matt@zukowski.ca>
2011-06-17 01:46:31 -0400

    started playing around with Fragments, but not really working :(

--- src/org/encorelab/sail/evoroom/ObservationsActivity.java
+++ src/org/encorelab/sail/evoroom/ObservationsActivity.java
@@ -1,48 +1,26 @@
 package org.encorelab.sail.evoroom;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.ViewFlipper;
 
 public class ObservationsActivity extends Activity {
-	ViewFlipper flipper;
-	Spinner colourPicker = null;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        FragmentManager.enableDebugLogging(true);
+        
         setContentView(R.layout.observations);
         
-        setupFlipper();
         
-        colourPicker = (Spinner) findViewById(R.id.colour_picker);
-	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-	            this, R.array.colours, android.R.layout.simple_spinner_item);
-	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-	    colourPicker.setAdapter(adapter);
+        
+        
+        Intent intent = new Intent(); 
+        intent.setClass(this, ObservationsEntryFragment.class); 
+        startActivity(intent);
+        
     }
-	
-	private void setupFlipper() {
-		flipper = (ViewFlipper) findViewById(R.id.observations);
-        
-        Button button_to_next = (Button) findViewById(R.id.button_to_prev);
-        Button button_to_prev = (Button) findViewById(R.id.button_to_next);
-        
-        button_to_prev.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				flipper.showPrevious();
-			}
-		});
-        
-        button_to_next.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				flipper.showNext();
-			}
-		});
-	}
 }
