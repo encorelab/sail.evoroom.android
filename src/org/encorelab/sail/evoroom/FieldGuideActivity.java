04aeb735c3a2712ec64e7df361cf941374f6ba7c
Colin McCann <colindmccann@gmail.com>
2011-06-23 14:05:48 -0400

    Field guide - finished, minus data entry and Michelle's inevitable changes

--- src/org/encorelab/sail/evoroom/FieldGuideActivity.java
+++ src/org/encorelab/sail/evoroom/FieldGuideActivity.java
@@ -331,31 +331,36 @@ public class FieldGuideActivity extends Activity {
 	        });
 			f2.setOnClickListener(new View.OnClickListener() {
 	            public void onClick(View v) {
-	            	speciesSelected = "";
+	            	speciesSelected = "leaf_monkey";
+	            	setupSpeciesDisplayView();
 	            	changeVis(R.id.field_guide_species_display);
 	            	}
 	        });
 			f3.setOnClickListener(new View.OnClickListener() {
 	            public void onClick(View v) {
-	            	speciesSelected = "";
+	            	speciesSelected = "malayan_tapir";
+	            	setupSpeciesDisplayView();
 	            	changeVis(R.id.field_guide_species_display);
 	            	}
 	        });
 			f4.setOnClickListener(new View.OnClickListener() {
 	            public void onClick(View v) {
-	            	speciesSelected = "";
+	            	speciesSelected = "sumatran_orangutan";
+	            	setupSpeciesDisplayView();
 	            	changeVis(R.id.field_guide_species_display);
 	            	}
 	        });
 			f5.setOnClickListener(new View.OnClickListener() {
 	            public void onClick(View v) {
-	            	speciesSelected = "";
+	            	speciesSelected = "sunda_leopard";
+	            	setupSpeciesDisplayView();
 	            	changeVis(R.id.field_guide_species_display);
 	            	}
 	        });
 			f6.setOnClickListener(new View.OnClickListener() {
 	            public void onClick(View v) {
-	            	speciesSelected = "";
+	            	speciesSelected = "sumatran_rhino";
+	            	setupSpeciesDisplayView();
 	            	changeVis(R.id.field_guide_species_display);
 	            	}
 	        });
@@ -373,11 +378,10 @@ public class FieldGuideActivity extends Activity {
             }
         });
 
-		ImageView image = (ImageView) findViewById(R.id.speciesPhoto);
-		TextView commonName, scientificName, family, description, habitat, prevelence;
-		TextView commonNameContent, scientificNameContent, familyContent, descriptionContent, habitatContent, prevelenceContent; 
-		commonName = (TextView) findViewById(R.id.commonNameField);
-		commonNameContent = (TextView) findViewById(R.id.commonNameContent);
+		ImageView speciesPhoto = (ImageView) findViewById(R.id.speciesPhoto);
+		ImageView speciesDrawn = (ImageView) findViewById(R.id.speciesDrawn);
+		TextView commonName, scientificName, family, description, habitat, prevalence, interesting, sources;
+		TextView commonNameContent, scientificNameContent, familyContent, descriptionContent, habitatContent, prevalenceContent, interestingContent, sourcesContent; 
 		scientificName = (TextView) findViewById(R.id.scientificNameField);
 		scientificNameContent = (TextView) findViewById(R.id.scientificNameContent);
 		family = (TextView) findViewById(R.id.familyField);
@@ -386,21 +390,188 @@ public class FieldGuideActivity extends Activity {
 		descriptionContent = (TextView) findViewById(R.id.descriptionContent);
 		habitat = (TextView) findViewById(R.id.habitatField);
 		habitatContent = (TextView) findViewById(R.id.habitatContent);
-		prevelence = (TextView) findViewById(R.id.prevelenceField);
-		prevelenceContent = (TextView) findViewById(R.id.prevelenceContent);
+		prevalence = (TextView) findViewById(R.id.prevalenceField);
+		prevalenceContent = (TextView) findViewById(R.id.prevalenceContent);
+		interesting = (TextView) findViewById(R.id.interestingField);
+		interestingContent = (TextView) findViewById(R.id.interestingContent);
+		sources = (TextView) findViewById(R.id.sourcesField);
+		sourcesContent = (TextView) findViewById(R.id.sourcesContent);
+
+		scientificNameContent.setText(" ");
+		familyContent.setText(" ");
+		descriptionContent.setText(" ");
+		habitatContent.setText(" ");
+		prevalenceContent.setText(" ");
+		interestingContent.setText(" ");
+		sourcesContent.setText(" ");
+		
+		if (speciesSelected.equals("rafflesia_arnoldii")) {
+			
+		}
+		if (speciesSelected.equals("amorphophallus_titanum")) {
+			
+		}
+		if (speciesSelected.equals("ficus_elastica")) {
+			
+		}
+		if (speciesSelected.equals("ficus_benjamina")) {
+			
+		}
+		if (speciesSelected.equals("ficus_microcarpa")) {
+			
+		}
+		if (speciesSelected.equals("tetrastigma")) {
+			
+		}
+		if (speciesSelected.equals("strangler_fig")) {
+			
+		}
+		if (speciesSelected.equals("amorphophallus_titanum")) {
+			
+		}
 		
 		if (speciesSelected.equals("striped_rabbit")) {
-			image.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
-			commonNameContent.setText("Sumatra short-eared rabbit");
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
 			scientificNameContent.setText("Nesolagus netscheri");
 			familyContent.setText("Leporidae");
-			descriptionContent.setText("The Sumatran Striped Rabbit is usually about 40 cm (1 ft, 4 in) long. It is gray with brown stripes, with a red tail and rump, and the underside is white. It lives in forests at altitudes of 600-1400 metres. It is nocturnal, resting in the burrows of other animals. It usually eats the stalk and leaves of understory plants, but captive rabbits ate grain, and tropical fruits");
+			descriptionContent.setText("The Sumatran Striped Rabbit is usually about 40 cm (1 ft, 4 in) long. It is gray with brown stripes, with a red tail and rump, and the underside is white. It lives in forests at altitudes of 600-1400 metres. It is nocturnal, resting in the burrows of other animals. It usually eats the stalk and leaves of understory plants, but captive rabbits ate grain and tropical fruits.");
 			habitatContent.setText("Exists in forests along the mountainous edge of Sumatra");
-			prevelenceContent.setText("Critically endangered because of loss of habitat");
-//			image.setImageDrawable(getResources().getDrawable(R.drawable.cartoon_striped_rabbit));
+			prevalenceContent.setText("Critically endangered because of loss of habitat");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("leaf_monkey")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("Presbytis melalophos");
+			familyContent.setText("Cercopithecidae");
+			descriptionContent.setText("The pollex (thumb) is reduced in this species. The orbits are widely spaced and the hindlimbs are longer as compared to the forelimbs. The average body mass for an adult male mitered leaf-monkey is around 5.9 kilograms, and for the female it is around 5.8 kilograms. This species has a sacculated stomach to assist in the breakdown of cellulose. Infants have a white pelage color with a dark stripe down the back and across the shoulders.");
+			habitatContent.setText("This species lives in primary lowland rainforests, hill forests, inland secondary forests, and submontane forests.");
+			prevalenceContent.setText("The species is considered Endangered due to ongoing population declines driven by habitat conversion and illegal collecting for the pet trade. Although forest loss has probably exceeded 70% over 3 generations (30 years approximately), the subspecies is tolerant to some degree of conversion such that it is likely to persist in fragmented landscapes over the medium term.");
+			sourcesContent.setText("Primata");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("malayan_tapir")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("Tapirus indicus");
+			familyContent.setText("Tapiridae");
+			descriptionContent.setText(Html.fromHtml("Also known as the Asian Tapir, the Malayan Tapir (Tapirus indicus) is the largest of the four species of tapir and the only one native to Asia. In the Malay language, the tapir is commonly referred to as \"cipan\", \"tenuk\" or \"badak tampong\".<br/>Notable markings: light colored patch which extends from its shoulders to its rear; the rest of its hair is black and tips of ears white; The pattern is for camouflage;<br/>-Grows to 6-8 feet (1.8 to 2.4 m) in length; stand 3-3.5 feet (90 to 107 cm tall); Females are larger than males<br/>-Poor eyesight but excellent sense of smell<br/>-Communicate with high-pitched squeaks and whistles<br/>-Live near water (bathe and swim often)<br/>-Can climb steep slopes<br/>-Active at night, eating soon after sunset or before sunrise"));
+			habitatContent.setText("Found in lowland rainforests");
+			prevalenceContent.setText("Deforestation for agricultural purposes, flooding caused by damming of rivers for hydroelectric projects and illegal trade");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("sumatran_orangutan")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("Pongo abelii");
+			familyContent.setText("Hominidae");
+			descriptionContent.setText(Html.fromHtml("One of two species of orangutans - it is rarer and smaller than Bornean orangutan. The Sumatran and Bornean species diverged 400,000 years ago, but the two islands split at least 21,000 years ago.<br/>-Red-orange coat of hair, and white around the mouth<br/>-Grows to height of 1.4 metres (4.6 ft) tall and 90 kilograms (200 lbs) in males; females smaller 90 centimetres (3.0 ft) and 45 kilograms (99 lbs)<br/>-Tends to be more frugivorous (figs and jackfruits) and insectivorous; it will also eat birds' eggs and small vertebrates<br/>-Observed using tools (breaking tree branches and uses them to dig in tree holes for termites or poke bees nest walls for honey, or to eat fruit<br/>-More social than Bornean orangutan"));
+			habitatContent.setText("Lives in rainforest and swamps of Sumatra (Suaq Balimbing swamp); largest of the tree-dwelling primates");
+			prevalenceContent.setText("7,3000 Sumatran organutans still live in the wild; critically endangered; no more than 6-7 orangutan's per square kilometer");
+			sourcesContent.setText("Wikipedia, Nature wildlife, Sumatra Orangutan Chapter 2, Know your Orangutan, Primatology");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("sunda_leopard")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("Neofelis diardi");
+			familyContent.setText("Felidae");
+			descriptionContent.setText("The Sunda Clouded Leopard, also known as the Sundaland Clouded Leopard and the Bornean clouded leopard is a medium-sized wild cat found in Borneo and Sumatra. The Sunda clouded leopard is the largest felid in Borneo, and has a stocky build, weighing around 12 to 25 kg (26 to 55 lb). The canine teeth are two inches long, which, in proportion to the skull length, are longer than those of any other extant feline. Its tail can grow to be as long as its body, aiding balance. Its coat is marked with irregularly-shaped, dark-edged ovals which are said to be shaped like clouds, hence its common name. In 2006, it was classified as a separate species, distinct from its continental relative Neofelis nebulosa.");
+			habitatContent.setText("Sunda clouded leopards are largely unknown because of their secretive nature. In Borneo, they occur in lowland rainforest, and at lower density, in logged forest. Records in Borneo are below 1,500 m (4,900 ft). In Sumatra, they appear to be more abundant in hilly, montane areas. It is unknown if there are still Sunda clouded leopards on the small Batu Islands close to Sumatra.");
+			prevalenceContent.setText("In 2008, the IUCN classified the species as vulnerable, with a total effective population size suspected to be fewer than 10,000 mature individuals, and a decreasing population trend.");
+			sourcesContent.setText("New Species Declared: Clouded Leopard found on Borneo and Sumatra, Wikipedia");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("sumatran_rhino")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("Dicerorhinus sumatrensis");
+			familyContent.setText("Rhinocerotidae");
+			descriptionContent.setText("The Sumatran rhino lives an estimated 30-45 years in the wild. They are solitary creatures except when pairing prior to mating and during child rearing. They are most active when eating at dawn or just after dusk, loves to wallow in mud baths to cool down and rest. In rainy seasons, the rhinos will move to higher elevations and in cooler months in the lower areas. The Sumatran Rhinos are fast and agile; they climb mountains easily and comfortably traverses steep slopes and riverbanks.");
+			habitatContent.setText("Sumatran Rhinos live in both lowland and highland secondary rainforest, swamps and the cloud forest. They prefer hilly areas close to water, particularly steep upper valleys with a lot of undergrowth. Known to be in Sumatra, Sabah and Borneo (used to live more places; only six known communities contain Sumatran Rhinos)");
+			prevalenceContent.setText("Fewer than 275 individuals remain; critically endangered primarily due to illegal poaching; rhino horns used in amulets and protection; rhino horn now seen as aphrodisiac (traditional Chinese medicine never used it for this purpose); Also hardwood from the rainforest causing rhino habitat depletion.");
+			interestingContent.setText("The most vocal of rhinoceros species; unknown purpose of vocalization (whistle blow)");
+			sourcesContent.setText("Wikipedia");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("sumatran_tiger")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("Panthera tigris sumatrae");
+			familyContent.setText("Felidae");
+			descriptionContent.setText(Html.fromHtml("The Sumatran tiger is the smallest of the tiger subspecies. Sumatran male tigers average 8 feet (2.438m) in length and weigh about 265 lbs. (120.2kg), while females average 7 feet (2.134m) in length and weigh about 200 lbs (90.718kg). Their small size helps them move through the jungle, and stripes and patterning is adapted for camouflage in the tall grass and underbush. Males are more bearded and maned (neck and cheek is developed).<br/>-They have webbed toes and are fast swimmers - they run their prey into the water<br/>-White spots on the back of tiger's ears are called \"eye spots\" or \"predator spots\". These spots are believed to function as false eyes as well as to make it look larger to any predator approaching from behind. This is particularly helpful in keeping cubs safe.<br/>-Sumatra provides a varying landscape in which this subspecies lives. From peat-moss forest, submountain and mountain forest as well as lowland forest. As a result, the Sumatran Tiger's coat has evolved slightly different from the other tiger subspecies. This evolution adaptation to their environment give the them the darkest coat of all tigers. The Sumatran Tiger has wide black stripes that are closely spaced, sometimes doubled up on each other. These stripes also run down the animal's forelegs, a distinction setting them apart from the Siberian Tiger.<br/>-They prey on larger ungulates, Wild Boar, Malayan Tapir, deer, smaller animals fowl, monkeys, fish, and mice (when desperate for food)."));
+			habitatContent.setText("Only found on Indonesian island of Sumatra, lowland forest to sub mountain and mountain forest including some peat moss forests; The agriculture of Sumatra has changed and thus caused problems for the tigers.");
+			prevalenceContent.setText("Continual loss of habitat (due for the deforestation from the production of palm oil), agricultural growth has made habitat for tigers fragmented. There are no more than 500 tigers left in the wild.");
+			sourcesContent.setText("Wikipedia, Tiger Homes");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("fig_wasp_sumatra")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("Eupristina verticillata");
+			familyContent.setText("Agaonidae");
+			descriptionContent.setText("Also known as the Chinese banyan pollinator wasp, the Eupristina verticillata is a sub species of fig wasps in from the Agaonidae family. They pollinate figs or are otherwise associated with figs in a coevolutional relationship that has been developing for at least 80 million years.");
+			sourcesContent.setText("Bugs in the news, Wikipedia");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("fig_wasp_borneo")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("");
+			familyContent.setText("");
+			descriptionContent.setText("");
+			habitatContent.setText("");
+			prevalenceContent.setText("");
+			sourcesContent.setText("");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("rhino_hornbill")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("");
+			familyContent.setText("");
+			descriptionContent.setText("");
+			habitatContent.setText("");
+			prevalenceContent.setText("");
+			sourcesContent.setText("");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("rhinoplax_vigil")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("");
+			familyContent.setText("");
+			descriptionContent.setText("");
+			habitatContent.setText("");
+			prevalenceContent.setText("");
+			sourcesContent.setText("");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("grey_hornbill")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("");
+			familyContent.setText("");
+			descriptionContent.setText("");
+			habitatContent.setText("");
+			prevalenceContent.setText("");
+			sourcesContent.setText("");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("proboscis_monkey")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("");
+			familyContent.setText("");
+			descriptionContent.setText("");
+			habitatContent.setText("");
+			prevalenceContent.setText("");
+			sourcesContent.setText("");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
+		}
+		if (speciesSelected.equals("borneo_orangutan")) {
+			speciesPhoto.setImageDrawable(getResources().getDrawable(R.drawable.striped_rabbit));
+			scientificNameContent.setText("");
+			familyContent.setText("");
+			descriptionContent.setText("");
+			habitatContent.setText("");
+			prevalenceContent.setText("");
+			sourcesContent.setText("");
+			speciesDrawn.setImageDrawable(getResources().getDrawable(R.drawable.drawn_sumatran_rabbit));
 		}
 
+
 		
+
+
 	}
 
 	private void changeVis(int vis) {
